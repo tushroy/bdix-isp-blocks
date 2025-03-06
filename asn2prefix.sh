7812dd2943c20d58dbf771bef04c831622a1fc8a
Tushar Roy <skydel.net@gmail.com>
2025-03-06 11:00:55 +0600

    updated

--- asn2prefix.sh
+++ asn2prefix.sh
@@ -56,7 +56,7 @@ rm -f asn*.html
 grep -oP 'AS\K([0-9]+)' ../bdix-asn-list.txt | xargs -P8 -I {} curl -s -o asn{}.html "https://www.dan.me.uk/bgplookup?asn={}&include_downstream=on"
 
 # Extracting IPv4 and IPv6 prefixes
-html2text asn*.html | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,2}/[0-9]{1,2}\b" > ../bdix-prefix_ipv4.txt
-html2text asn*.html | grep -oE "\b([0-9a-fA-F:]+:+)+(:[0-9a-fA-F]+)?/[0-9]{1,3}\b" > ../bdix-prefix_ipv6.txt
+html2text -ascii asn*.html | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,2}/[0-9]{1,2}\b" > ../bdix-prefix_ipv4.txt
+html2text -ascii asn*.html | grep -oE "\b([0-9a-fA-F:]+:+)+(:[0-9a-fA-F]+)?/[0-9]{1,3}\b" > ../bdix-prefix_ipv6.txt
 
 echo "Script execution completed successfully."
