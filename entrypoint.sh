b68dde8d2bd4aaaabfdb8f95c220a488bcdc6978
Tushar Roy <skydel.net@gmail.com>
2025-03-06 09:54:06 +0600

    with timezone information

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@
 env >> /etc/environment
 
 cat /crontab > /var/spool/cron/crontabs/root
-
+CURRENT_DATETIME=$(date +"%Y-%m-%d %H:%M %Z")
+echo "Startup date time: $CURRENT_DATETIME"
 # execute CMD
 echo "$@"
 exec "$@"
