9adc0faddf7156dbc6adae64e1a1ba8b593a0ca9
Tushar Roy <tushroy@users.noreply.github.com>
2025-12-06 13:36:07 +0600

    Update asn2prefix.sh
    from fixed asn list

--- asn2prefix.sh
+++ asn2prefix.sh
@@ -8,7 +8,7 @@ cd asn || { echo "Failed to enter 'asn' directory"; exit 1; }
 rm -f asn*.html
 
 # Fetch BDIX ASNs from Hurricane Electric BDIX overview page.
-curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36" -s "https://bgp.he.net/exchange/BDIX" | lynx -stdin -dump | grep -oE "\bAS[0-9]{1,6}\b" | sort -u > ../bdix-asn-list.txt
+# curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36" -s "https://bgp.he.net/exchange/BDIX" | lynx -stdin -dump | grep -oE "\bAS[0-9]{1,6}\b" | sort -u > ../bdix-asn-list.txt
 
 # Fetching ASN data
 grep -oP 'AS\K([0-9]+)' ../bdix-asn-list.txt | xargs -P8 -I {} curl -s -o asn{}.html "https://www.dan.me.uk/bgplookup?asn={}&include_downstream=on"
